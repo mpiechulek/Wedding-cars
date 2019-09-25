fa7ccfbf9bd866699ed15327d74a4093a6e77b12
MP <47081376+mpiechulek@users.noreply.github.com>
2019-09-25 12:58:21 +0200

    fixed background

---
+++ src/js/ham-menu.js
@@ -0,0 +1,9 @@
+//jshint esversion:6
+
+export const openMenu = () => {
+    document.getElementById('menu').style.width = '25rem';
+};
+
+export const closeMenu = () => {
+    document.getElementById('menu').style.width = '0';
+};
